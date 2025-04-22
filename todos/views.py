f5c870284f666f21dd1e82e840cc420fc0697b6f
vladimirca2000 <vladimirca2000@gmail.com>
2025-04-21 21:01:24 -0300

    Insert na mão para visualizar - (1:09:01) do video

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Todo
 
 
-# Create your views here.
-def home(request):
-    return render(request, "todos/home.html")
+def todo_list(request):
+    todos = Todo.objects.all()
+    return render(request, "todos/todo_list.html", {"todos": todos})
+
+
+#         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
